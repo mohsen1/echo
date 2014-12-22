934c19c141c5af249e2252fcbb79fa126b71fd4d
Mohsen Azimi <msn.azi@gmail.com>
2014-12-22 11:02:04 -0800

    Echo body

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var app = require('express')();
 var port = process.env.PORT || 3300;
+var concat = require('concat-stream');
 
-
-
+app.use(function(req, res, next) {
+  req.pipe(concat(function(data){
+    req.body = data;
+    next();
+  }));
+});
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
